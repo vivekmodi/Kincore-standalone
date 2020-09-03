6805a8e3ac5b4111f92718b00422ebe9d29c60f7
Vivek Modi <modi.vivek2009@gmail.com>
2020-09-03 12:40:44 -0400

    Added option to provide multiple chain ids

--- kinase_state.py
+++ kinase_state.py
@@ -38,26 +38,27 @@ def identify_state(pwd,pdbfilename,align,user_model,user_chain,user_lys,user_glu
     structure=parser.get_structure(pdbfilename, handle)
         
     if align.upper()=='FALSE':
-        index=int(user_model)
-        conf_df.at[index,'Model_id']=str(user_model)
-        conf_df.at[index,'Chain_id']=str(user_chain)
-        conf_df=assign_default_values(index,conf_df)
-        conf_df.at[index,'Lys_num']=int(user_lys)
-        conf_df.at[index,'Glu_num']=int(user_glu)
-        conf_df.at[index,'Glu4_num']=int(user_glu)+4
-        conf_df.at[index,'Phe_num']=int(user_phe)
-        conf_df.at[index,'XDFG_num']=int(user_phe)-2
-        conf_df.at[index,'Asp_num']=int(user_phe)-1
-        conf_df=identify_restypes(pdbfilename,conf_df,index,structure)
-        conf_df=compute_distance(pdbfilename,index,conf_df,structure)
-        conf_df=compute_dihedrals(pdbfilename,index,conf_df,structure)
-        conf_df=chelix_conformation(index,conf_df)
-        conf_df=spatial_label(index,conf_df)
-        conf_df=dihedral_label(index,conf_df,0.45)
-        conf_df=chelix_conformation(index,conf_df)
         print('Input'.rjust(13)+'Model'.rjust(6)+'Chain'.rjust(6)+'B3-Lys'.rjust(7)+'C-helix-Glu'.rjust(12)+'DFG-Phe'.rjust(8)+'Spatial_label'.rjust(14)+\
                                   'Dihedral_label'.rjust(15)+'C-helix_label'.rjust(14))
-        print(pdbfilename.rjust(13)+conf_df.at[index,'Model_id'].rjust(6)+conf_df.at[index,'Chain_id'].rjust(6)+\
+        index=int(user_model)
+        conf_df.at[index,'Model_id']=str(user_model)
+        for chains in list(user_chain.split(',')):
+            conf_df.at[index,'Chain_id']=chains
+            conf_df=assign_default_values(index,conf_df)
+            conf_df.at[index,'Lys_num']=int(user_lys)
+            conf_df.at[index,'Glu_num']=int(user_glu)
+            conf_df.at[index,'Glu4_num']=int(user_glu)+4
+            conf_df.at[index,'Phe_num']=int(user_phe)
+            conf_df.at[index,'XDFG_num']=int(user_phe)-2
+            conf_df.at[index,'Asp_num']=int(user_phe)-1
+            conf_df=identify_restypes(pdbfilename,conf_df,index,structure)
+            conf_df=compute_distance(pdbfilename,index,conf_df,structure)
+            conf_df=compute_dihedrals(pdbfilename,index,conf_df,structure)
+            conf_df=chelix_conformation(index,conf_df)
+            conf_df=spatial_label(index,conf_df)
+            conf_df=dihedral_label(index,conf_df,0.45)
+            conf_df=chelix_conformation(index,conf_df)
+            print(pdbfilename.rjust(13)+conf_df.at[index,'Model_id'].rjust(6)+conf_df.at[index,'Chain_id'].rjust(6)+\
                               str(int(conf_df.at[index,'Lys_num'])).rjust(7)+str(int(conf_df.at[index,'Glu_num'])).rjust(12)+str(int(conf_df.at[index,'Phe_num'])).rjust(8)+conf_df.at[index,'Spatial_label'].rjust(14)+\
                               conf_df.at[index,'Dihedral_label'].rjust(15)+conf_df.at[index,'Chelix'].rjust(14))
     
@@ -121,12 +122,13 @@ if __name__ == '__main__':
     parser.add_argument('PDB',help='PDB file in .cif or .pdb format. You can also provide compressed file as file.cif.gz or file.pdb.gz')
     parser.add_argument('Align',help='True/False; If "True" then the program will align the input protein sequence to pre-computed HMMs and identify conserved \
                         residues (Example $python kinase_state.py 1GAG.pdb True).\
-                        If "False" then the please provide the optional arguments (Example $python kinase_state.py --Model 0 --Chain A --Lys 1030 --Glu 1047 --Phe 1151 1GAG.pdb False)')
-    parser.add_argument('--Model',help='Model id of the protein kinase structure in the input file, if the file does not contain any Model id then specify 0')
-    parser.add_argument('--Chain',help='Chain id of the protein kinase chain in the input file')
-    parser.add_argument('--Lys',help='Residue number of the conserved B3-Lys in the input PDB file')
-    parser.add_argument('--Glu',help='Residue number of the conserved C-helix-Glu in the input PDB file')
-    parser.add_argument('--Phe',help='Residue number of the conserved DFG-Phe in the input PDB file')
+                        If "False" then the please provide the optional arguments (Example $python kinase_state.py --Model 0 --Chain A,B,C --Lys 1030 --Glu 1047 --Phe 1151 1GAG.pdb False)')
+    
+    parser.add_argument('-L','--Lys',help='Residue number of the conserved B3-Lys in the input PDB file')
+    parser.add_argument('-G','--Glu',help='Residue number of the conserved C-helix-Glu in the input PDB file')
+    parser.add_argument('-P','--Phe',help='Residue number of the conserved DFG-Phe in the input PDB file')
+    parser.add_argument('-M','--Model',help='Model id of the protein kinase structure in the input file, if the file does not contain any Model id then specify 0')
+    parser.add_argument('-C','--Chain',type=str,help='Chain id of the protein kinase chain in the input file')
     args=parser.parse_args()
    
     pdbfilename=args.PDB
@@ -134,6 +136,6 @@ if __name__ == '__main__':
         user_model=str(args.Model);user_chain=str(args.Chain);user_lys=int(args.Lys);user_glu=int(args.Glu);user_phe=int(args.Phe)
     if args.Align=='True':    
         user_model=user_chain=user_lys=user_glu=user_phe=align=''
-   
+    
     identify_state(hmm_loc,pdbfilename,args.Align,user_model,user_chain,user_lys,user_glu,user_phe)
     
